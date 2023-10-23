15f7a985b57e27b3c651e8b36bfed5da5859eb07
TheRhicrosoradile1 <f20170768@hyderabad.bits-pilani.ac.in>
2023-10-23 16:34:09 +0530

    boxandunbox

---
+++ java_1/boxandunbox.java
@@ -0,0 +1,8 @@
+public class boxandunbox {
+    public static void main(String[] args){
+        String name = "koko";
+        int age = 21;
+        System.out.println("Hello,"+ name+"! You are "+age+" years old.");
+
+    }
+}
