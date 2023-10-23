0a3f5c864baec046acd367cd00afd5ec9539b9eb
TheRhicrosoradile1 <f20170768@hyderabad.bits-pilani.ac.in>
2023-10-23 15:46:28 +0530

    [ASSIGNMENT -2 ]

---
+++ java_1/datatype.java
@@ -0,0 +1,15 @@
+public class datatype{
+    public static void main(String[] args){
+        int intval = 42;
+        double dobval = 3.14159;
+        char charval = 'A';
+        boolean boolval = true;
+        String strval = "Hello, SkillCaptain!";
+        System.out.println("int val  :"+intval);
+        System.out.println("dob val  :"+dobval);
+        System.out.println("char val  :"+charval);
+        System.out.println("bool val  :"+boolval);
+        System.out.println("strval val  :"+strval);
+
+    }
+}
